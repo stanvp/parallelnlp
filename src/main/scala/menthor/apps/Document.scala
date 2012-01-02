620b5b375163e9d93085ad02b6931e0abad01d4f
Stanislav Peshterliev <speshterliev@gmail.com>
2012-01-02 17:57:51 +0200

    Changed sample definition

--- src/main/scala/menthor/apps/Document.scala
+++ src/main/scala/menthor/apps/Document.scala
@@ -3,9 +3,8 @@ package menthor.apps
 import menthor.classifier.Sample
 
 case class Document(name: String, categories: List[String], termFrequency: Map[String, Double]) extends Sample {
-  val totalTerms = termFrequency.values.sum
   override def toString()  = "Document(%s,%s)".format(name, categories)
   
-  override def total: Double = totalTerms
-  override def features : Map[String, Double] = termFrequency
+  val total: Double = termFrequency.values.sum
+  val features : Map[String, Double] = termFrequency
 }
